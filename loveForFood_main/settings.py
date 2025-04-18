30a27ca6729f2d0cffecac74410f0c1466f65038
Naomi_Jalil <110171806+Naomi-Jalil@users.noreply.github.com>
2025-04-18 15:36:33 +0600

    Installed gunicorn for deployment

--- loveForFood_main/settings.py
+++ loveForFood_main/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['139.162.162.140', '127.0.0.1',
+                 'loveforfood.ml', 'www.loveforfood.ml']
 
 
 # Application definition
